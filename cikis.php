2f47277c44e9c1886b3d0821db4c5b7622cecfd9
onurkorrkmaz <166871842+onurkorrkmaz@users.noreply.github.com>
2024-04-13 16:22:52 +0300

    eski düzene geçtim.

--- cikis.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['login']);
-echo " Başarıyla çıkış yapıldı. <br> Giriş sayfasına yönlendiriliyorsunuz.";
-header ("refresh:2 ; url=index.php");
-?>
--- vtbaglan.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$baglanti=mysqli_connect('localhost','root','','randevu');
-if(!$baglanti) {
-echo "Veri tabanına bağlanılamadı ".mysqli_connect_error();
-
-}
-
-// mysqli_set_charset($baglanti,"uft");
-?>
